a03c70657ba435b8848a94738e8baafcf57a9a05
alberto-david <albertodavid254@gmail.com>
2022-10-25 03:12:58 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-'''
-Write a function that reads a text file (UTF8)
-and prints it to stdout
-'''
+""" Module that contains a function that reads from a file """
 
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
-    f.close()
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')

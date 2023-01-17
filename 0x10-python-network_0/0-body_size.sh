edb0ac80922960d9e9d631537561f8f02163239b
alberto-david <albertodavid254@gmail.com>
2023-01-17 01:15:54 -0800

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-# Script that shows the Content-Length from a HTTP request
-curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
-=======
 # takes in a URL, sends a request to that URL, displays the size of the body of the response
 curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
->>>>>>> e02b7f3bdf042f2c0e0e623bdf75c323c9d83d8c
